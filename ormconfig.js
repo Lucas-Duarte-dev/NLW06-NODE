048ec1bea9265b70bb7b58703e2b0adc9d2becd4
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-06-21 21:19:06 -0300

    NLW06-NODE: Run migrations

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,7 @@
 module.exports = {
   type: "sqlite",
   database: "src/database/database.sqlite",
+  migrations: ["src/database/migrations/*.ts"],
   cli: {
     migrationsDir: "src/database/migrations",
   },
