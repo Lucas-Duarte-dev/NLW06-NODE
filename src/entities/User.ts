95330ba10bbdfa58410fa28fcf4d90d5099bbec3
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-06-21 21:27:11 -0300

    NLW06-NODE: Create user model

---
+++ src/entities/User.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+@Entity()
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { User };
