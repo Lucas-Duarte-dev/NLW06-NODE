b1671022e784274e7b0efd1dbee36123bba96fe5
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-06-23 22:32:29 -0300

    NLW06-NODE: Created complements migration

---
+++ src/database/migrations/1624497040037-CreateComplements.ts
@@ -0,0 +1,70 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateComplements1624497040037 implements MigrationInterface {
+  protected table_name = "complements";
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: this.table_name,
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "user_sender",
+            type: "uuid",
+          },
+          {
+            name: "user_receiver",
+            type: "uuid",
+          },
+          {
+            name: "tag_id",
+            type: "uuid",
+          },
+          {
+            name: "message",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKUserSenderComplements",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_sender"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+          {
+            name: "FKUserReceiverComplements",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_receiver"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+          {
+            name: "FKTagComplements",
+            referencedTableName: "tags",
+            referencedColumnNames: ["id"],
+            columnNames: ["tag_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.table_name);
+  }
+}
