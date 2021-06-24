fb036eed793ab322a51e4867c739903d55d82fe7
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-06-23 22:42:32 -0300

    NLW06-NODE: Created complements entity

---
+++ src/entities/Complement.ts
@@ -0,0 +1,56 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+import { Tag } from "./Tag";
+import { User } from "./User";
+
+@Entity("tags")
+class Complement {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  user_sender: string;
+
+  @JoinColumn({ name: "user_sender" })
+  @ManyToOne(() => User)
+  userSender: User;
+
+  @Column()
+  user_receiver: string;
+
+  @JoinColumn({ name: "user_receiver" })
+  @ManyToOne(() => User)
+  userReceiver: User;
+
+  @Column()
+  tag_id: string;
+
+  @JoinColumn({ name: "tag_id" })
+  @ManyToOne(() => Tag)
+  tag: Tag;
+
+  @Column()
+  message: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Complement };
