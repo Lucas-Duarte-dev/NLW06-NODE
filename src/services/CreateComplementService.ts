1f84ea330a12b1ad55ccef398df0159d98b1b039
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-06-24 08:28:59 -0300

    NLW06-NODE: Created interface/service/controller

---
+++ src/services/CreateComplementService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from "typeorm";
+import { ICreateComplement } from "../dtos/ICreateComplement";
+import { Complement } from "../entities/Complement";
+import { ICreateComplementService } from "../interfaces/ICreateComplementService";
+import { ComplementRepositories } from "../repositories/ComplementRepositories";
+import { UserRepositories } from "../repositories/UserRepositories";
+
+class CreateComplementService implements ICreateComplementService {
+  async execute({
+    user_sender,
+    user_receiver,
+    tag_id,
+    message,
+  }: ICreateComplement): Promise<Complement> {
+    const complementRepository = getCustomRepository(ComplementRepositories);
+
+    const userRepositories = getCustomRepository(UserRepositories);
+
+    if (user_sender === user_receiver) {
+      throw new Error("Incorrect User Receiver");
+    }
+
+    const userReceiverExists = await userRepositories.findOne(user_receiver);
+
+    if (!userReceiverExists) {
+      throw new Error("User Receiver does not exists");
+    }
+
+    const complement = complementRepository.create({
+      tag_id,
+      user_receiver,
+      user_sender,
+      message,
+    });
+
+    await complementRepository.save(complement);
+
+    return complement;
+  }
+}
+
+export { CreateComplementService };
